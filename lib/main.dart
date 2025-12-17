844fcf8739e4c7a1755028176b6267c618b222aa
edgarpapikyan1997 <edpap7788@gmail.com>
2025-12-17 14:25:43 -0800

    updated go router

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:iron_byte/presentation/screens/home_screen.dart';
+import 'features/main/main_screen.dart';
 
 void main() {
   runApp(App());
 }
-
-
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: const HomeScreen());
+    return MaterialApp(home: MainScreen());
   }
 }
