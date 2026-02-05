aa8be1d5522da3c28d15febabb582fec1e8b19de
edgarpapikyan1997 <edpap7788@gmail.com>
2026-02-05 07:25:25 -0800

    updated web

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'core/router/app_router.dart';
 import 'core/theme/app_theme.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
   await EasyLocalization.ensureInitialized();
 
   runApp(
