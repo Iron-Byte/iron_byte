246ba74394137035f15564e1235a17ff7f532c25
edgarpapikyan1997 <edpap7788@gmail.com>
2026-04-07 21:23:09 +0200

    updated_test

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,14 @@
-import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'core/router/app_router.dart';
-import 'core/theme/app_theme.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await SystemChrome.setPreferredOrientations([
-    DeviceOrientation.portraitUp,
-    DeviceOrientation.portraitDown,
-  ]);
-  await EasyLocalization.ensureInitialized();
-
-  runApp(
-    EasyLocalization(
-      supportedLocales: [Locale('en')],
-      path: 'assets/translations',
-      fallbackLocale: Locale('en'),
-      child: App(),
-    ),
-  );
+void main() {
+  runApp(IronByteApp());
 }
 
-class App extends StatelessWidget {
-  const App({super.key});
+class IronByteApp extends StatelessWidget {
+  const IronByteApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      localizationsDelegates: context.localizationDelegates,
-      supportedLocales: context.supportedLocales,
-      locale: context.locale,
-      routerConfig: appRouter,
-      title: 'Iron Byte',
-      theme: AppTheme.theme,
-    );
+    return const Placeholder();
   }
 }
